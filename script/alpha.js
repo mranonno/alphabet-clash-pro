9382cb9979413746401840550897e4b5136fdf9a
mranonno <anonnodas97@gmail.com>
2024-02-15 11:57:44 +0600

    score screen complete

--- script/alpha.js
+++ script/alpha.js
@@ -5,8 +5,55 @@
 //     playGroundScreen.classList.remove('hidden');
 // }
 
+
+
 function play() {
     hideElementById('home-screen');
+    hideElementById('score-screen');
     showElementById('play-ground');
-    getRandomAlphabet();   
+    setInitialValue('current-life', 5);
+    setInitialValue('current-score', 0);
+    getRandomAlphabet(); 
 }
+document.addEventListener('keyup', function (event) {
+    const userPressed = event.key;
+    const currentAlphabet = document.getElementById('displaySection');
+    const displayAlphabet = currentAlphabet.innerText.toLowerCase();
+    if (userPressed === displayAlphabet) {
+        const initialScore = document.getElementById('current-score');
+        const scoreText = initialScore.innerText;
+        const scoreValue = parseInt(scoreText);
+        const newScore = scoreValue+1;
+        initialScore.innerText = newScore;
+        removeKeyboardBackground(userPressed);
+        getRandomAlphabet();
+    }
+    else {
+        const initialLife = document.getElementById('current-life')
+        const lifeText = initialLife.innerText;
+        const lifeValue = parseInt(lifeText);
+        const newLife = lifeValue - 1;
+        initialLife.innerText = newLife;
+        if (newLife === 0) {
+            gameOver();
+            removeKeyboardBackground(displayAlphabet);
+        }
+    }
+})
+function gameOver() {
+    hideElementById('play-ground');
+    showElementById('score-screen');
+    const lastScore = document.getElementById('current-score').innerText;
+    const finalResult = document.getElementById('final-result');
+    finalResult.innerText = lastScore;
+    
+}
+
+
+// function playAgain() {
+//     showElementById('play-ground');
+//     hideElementById('score-screen');
+//     setInitialValue('current-life', 5);
+//     setInitialValue('current-score', 0);
+
+// }
--- script/utility.js
+++ script/utility.js
@@ -1,23 +1,29 @@
 function hideElementById(elementId) {
     document.getElementById(elementId).classList.add('hidden');
 }
-
 function showElementById(elementId) {
     document.getElementById(elementId).classList.remove('hidden');
 }
 function setKeyboardBackground(elementId) {
     document.getElementById(elementId).classList.add('bg-amber-500')
 }
+function removeKeyboardBackground(elementId) {
+    document.getElementById(elementId).classList.remove('bg-amber-500')
+
+}
+function setInitialValue(element, value) {
+    const currentText = document.getElementById(element);
+    currentText.innerText = value;
+}
 function getRandomAlphabet() {
     // create random alphabet by array string
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('');
-    // create random number 0-25 and their round number
-    const randomNumber = Math.round(Math.random() * 25)
-    const randomAlphabet = alphabets[randomNumber]
+    // create random number 0-25(A-Z) and their round number
+    const randomNumber = Math.round(Math.random() * 25);
+    const randomAlphabet = alphabets[randomNumber];
     // show random alphabet in display
     const displayById = document.getElementById('displaySection');
     displayById.innerText = randomAlphabet;
     setKeyboardBackground(randomAlphabet);
-
 }
